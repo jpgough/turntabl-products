649dc991bda03705fe2a961f2847a3126fc72a3a
Dawud Ibrahim Ismail <ismaildawud96@gmail.com>
2020-05-20 08:09:25 +0000

    chores
    added swagger metadata for api & model description

--- src/main/java/com/turntabl/api/controller/ProductController.java
+++ src/main/java/com/turntabl/api/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.turntabl.api.controller;
 
 import com.turntabl.api.domain.Product;
 import com.turntabl.api.service.Data;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Extension;
+import io.swagger.annotations.ExtensionProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +24,19 @@ public class ProductController {
     private Data data;
 
     @GetMapping("/products")
+    @ApiOperation(
+            value = "Returns all products ",
+            notes = "Multiple products object values, separated by comma"
+    )
     public List<Product> getProducts() {
         return data.getProducts();
     }
 
     @GetMapping(value = "/products/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(
+            value = "Returns a product by an Id",
+            notes = "single products object value with matching id"
+    )
     public ResponseEntity<Product> getProduct(@PathVariable(value = "productId") String productId){
         Optional<Product> product = data.getProduct(productId);
         return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
